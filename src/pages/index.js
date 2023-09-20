d6144e30ee9ef29c7581a4f9ae138e06c0d8b4a2
Spencer Franklin <spencer.franklin@hotmail.com>
2023-09-19 17:09:37 -0700

    Second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function IndexPage() {
   return (
     <main>
       <h1>Coming soon!!!</h1>
+      <Link to='/about'>About this site</Link>
     </main>
   )
 }
